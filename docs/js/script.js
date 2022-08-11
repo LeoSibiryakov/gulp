62e68e82f4d3827fa8f754302f3f03e5f26da3bb
Ivan <example@mail.ru>
2022-08-11 19:49:27 +0500

    pre-final

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,109 +1,19 @@
-// $(document).ready(function(){ 
-
-//     //переключение слайдов по кнопкам
-
-//     $('.carousel__inner').slick({  
-//         speed: 1200,
-//         adaptiveHeight: true,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></img></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></img></button>',
-//         responsive: [
-//             {
-//               breakpoint: 992,
-//               settings: {
-//                 dots: true,
-//                 arrows: false
-//               }
-//             }
-//           ]
-//       });
-
-//     // выбор каталогов
-
-//     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() { 
-//        $(this)
-//          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-//          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-//     });
-
-//         function toggleSlide(item) {
-//           $(item).each(function(i) {
-//               $(this).on('click', function(e) {
-//                   e.preventDefault();
-//                   $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-//                   $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-//               })
-//           });
-//         };
-
-//         toggleSlide('.catalog-item__link');
-//         toggleSlide('.catalog-item__back');
-
-//     // модальные окна
-
-//     $('[data-modal=consultation]').on('click', function() {
-//         $('.overlay, #consultation').fadeIn();
-//     });
-
-//     $('.modal__close').on('click', function() {
-//         $('.overlay, #consultation, #thanks, #order').fadeOut();
-//     });
-
-//     $('.button_mini').each(function(i) {
-//       $(this).on('click', function() {
-//         $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-//         $('.overlay, #order').fadeIn();
-//       })
-//     }); 
-
-//       function validateForms(form) {
-//         $(form).validate({
-//         rules: {
-//           name: {
-//             required: true,
-//             minlenght: 2
-//           },
-//           phone: "required",
-//           email: {
-//             required: true,
-//             email: true
-//           }
-//         },
-//         messages: {
-//           name: {
-//             required: "Пожалуйста, введите своё имя",
-//             minlenght: jQuery.validator.format('Введите {0} символа!')
-//           },
-//           phone: "Пожалуйста, введите номер телефона",
-//           email: {
-//             required: "Пожалуйста, введите свою почту",
-//             email: "Неправильно введён адрес почты!"
-//           }
-//         }
-//       });
-//     };
-
-//   validateForms('#consultations-form');
-//   validateForms('#consultations form');
-//   validateForms('#order form');
-// });
-
 $(document).ready(function(){
   $('.carousel__inner').slick({
-      speed: 1200,
-      adaptiveHeight: true,
-      prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-      responsive: [
-          {
-              breakpoint: 992,
-              settings: {
-                  dots: true,
-                  arrows: false
-              }
-          }
-      ]
-  });
+    speed: 1200,
+    adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }
+    ]
+});
   
   $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
       $(this)
